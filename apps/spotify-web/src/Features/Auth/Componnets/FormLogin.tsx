680748ee46c1bcd256ffbdc5026cae1aa6975c24
moshaosama <mohamedOSFekry@gmail.com>
2025-06-03 21:46:03 +0300

    Modify FormLogin component in spotify-web

--- apps/spotify-web/src/Features/Auth/Componnets/FormLogin.tsx
+++ apps/spotify-web/src/Features/Auth/Componnets/FormLogin.tsx
@@ -1,7 +1,5 @@
-import { FcGoogle } from "react-icons/fc";
-import { LogoSpotify } from "../../../Contsants/LogoSpotify";
-import { MdFacebook } from "react-icons/md";
-import { FaApple } from "react-icons/fa";
+import { LogoSpotify } from "../../../Components/LogoSpotify";
+import SignWithSocialMedia from "../../../Components/SignWithSocialMedia";
 
 const FormLogin = () => {
   return (
@@ -16,33 +14,7 @@ const FormLogin = () => {
         <div>
           <h1 className="text-3xl font-bold">Log in to Spotify</h1>
         </div>
-        <div className="flex flex-col items-center gap-3 mt-5">
-          <div className="border-white hover:border-2 cursor-pointer border-1 w-72 flex gap-2 justify-around items-center p-2 rounded-full">
-            <div>
-              <FcGoogle size={20} />
-            </div>
-            <div>
-              <h1 className="font-bold">Continue with Google</h1>
-            </div>
-          </div>
-          <div className="border-white hover:border-2 cursor-pointer border-1 w-72 flex gap-2 justify-around items-center p-2 rounded-full">
-            <div>
-              <FaApple size={20} />
-            </div>
-            <div>
-              <h1 className="font-bold">Continue with Apple</h1>
-            </div>
-          </div>
-          <div className="border-white hover:border-2 cursor-pointer border-1 w-72 flex gap-2 justify-around items-center p-2 rounded-full">
-            <div>
-              <MdFacebook size={20} color="skyblue" />
-            </div>
-            <div>
-              <h1 className="font-bold">Continue with Facebook</h1>
-            </div>
-          </div>
-          <hr className="h-[1px] w-[30pc] my-5 border-none bg-gray-600" />
-        </div>
+        <SignWithSocialMedia />
         <form action="">
           <p className="flex flex-col gap-1">
             <label htmlFor="Email">Email or username</label>
