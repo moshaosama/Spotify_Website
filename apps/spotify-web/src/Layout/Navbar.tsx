2b0f1a66ba3e1899694718e624633833bcdc6cf4
moshaosama <mohamedOSFekry@gmail.com>
2025-06-03 23:04:19 +0300

    Update Navbar layout

--- apps/spotify-web/src/Layout/Navbar.tsx
+++ apps/spotify-web/src/Layout/Navbar.tsx
@@ -1,16 +1,32 @@
+import { CiSearch } from "react-icons/ci";
+import { FaBars } from "react-icons/fa";
 import { ImSpotify } from "react-icons/im";
 import { IoArrowDownCircleSharp } from "react-icons/io5";
 import { RiHome2Fill } from "react-icons/ri";
 import { Link } from "react-router";
+import Sidebar from "../Components/Sidebar";
+import { useOpenSidebar } from "../Context/OpenSidebar";
 
 const Navbar = () => {
+  const { handleTriggerSidebar } = useOpenSidebar();
   return (
     <>
-      <div className="p-5 flex justify-between">
+      <div className="p-5 flex items-center justify-between">
         <div>
           <ImSpotify className="text-white text-4xl w-fit" />
         </div>
-        <div>
+        <div className="sm:hidden">
+          <div className="flex items-center gap-4">
+            <CiSearch color="white" size={25} />
+            <button className="bg-white w-24 py-1 rounded-full font-bold">
+              Open App
+            </button>
+            <FaBars color="white" size={25} onClick={handleTriggerSidebar} />
+          </div>
+
+          <Sidebar />
+        </div>
+        <div className="max-sm:hidden">
           <div className="flex justify-between gap-5">
             <RiHome2Fill
               className="text-white p-2 text-5xl rounded-full w-fit cursor-pointer"
@@ -24,7 +40,7 @@ const Navbar = () => {
             />
           </div>
         </div>
-        <div className="font-bold text-[#7C7C7C]">
+        <div className="font-bold text-[#7C7C7C] max-sm:hidden">
           <div className="flex items-center gap-7">
             <h1 className="hover:text-white flex items-center gap-1 cursor-pointer transition-all duration-300">
               <IoArrowDownCircleSharp size={17} />
