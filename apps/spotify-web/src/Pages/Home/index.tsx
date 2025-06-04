59cc6c53da0ab4de132fedc50fda0685f3cd06b6
moshaosama <mohamedOSFekry@gmail.com>
2025-06-04 19:15:14 +0300

    Update Home page index.tsx in spotify-web

--- apps/spotify-web/src/Pages/Home/index.tsx
+++ apps/spotify-web/src/Pages/Home/index.tsx
@@ -1,6 +1,8 @@
 import Library from "../../Features/Home/Components/Library";
+import useSaveUser from "../../Hooks/useSaveUser";
 
 const Home = () => {
+  useSaveUser();
   return (
     <>
       <Library />
