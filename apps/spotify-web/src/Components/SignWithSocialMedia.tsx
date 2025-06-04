aacff7eda28b6795f4d66ec7546a3c30baeaac38
moshaosama <mohamedOSFekry@gmail.com>
2025-06-04 19:15:14 +0300

    Update SignWithSocialMedia.tsx component in spotify-web

--- apps/spotify-web/src/Components/SignWithSocialMedia.tsx
+++ apps/spotify-web/src/Components/SignWithSocialMedia.tsx
@@ -1,15 +1,17 @@
 import { FaApple } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 import { MdFacebook } from "react-icons/md";
+import useGoogleAuth from "../Features/Auth/Hooks/useGoogleAuth";
 
 const SignWithSocialMedia = () => {
+  const { handleClick } = useGoogleAuth();
   return (
     <div className="flex flex-col items-center gap-3 mt-5">
       <div className="border-white hover:border-2 cursor-pointer border-1 w-72 flex gap-2 justify-around items-center p-2 rounded-full">
         <div>
           <FcGoogle size={20} />
         </div>
-        <div>
+        <div onClick={handleClick}>
           <h1 className="font-bold">Continue with Google</h1>
         </div>
       </div>
