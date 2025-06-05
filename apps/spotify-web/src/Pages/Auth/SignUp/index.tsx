10f0ddf42b4b504d9cedeb853f56f8d6eb7d739b
moshaosama <mohamedOSFekry@gmail.com>
2025-06-06 01:53:11 +0300

    added: SignUp page folder

---
+++ apps/spotify-web/src/Pages/Auth/SignUp/index.tsx
@@ -0,0 +1,11 @@
+import FormSignUp from "../../../Features/Auth/Componnets/FormSignUp";
+
+const SignUp = () => {
+  return (
+    <>
+      <FormSignUp />
+    </>
+  );
+};
+
+export default SignUp;
