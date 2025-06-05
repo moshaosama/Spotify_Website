99b210b8a40fd55a33b4c2a8714abd79d952b2fd
moshaosama <mohamedOSFekry@gmail.com>
2025-06-06 00:56:51 +0300

    modified: AuthService.ts

--- apps/spotify-web/src/api/Auth/AuthService.ts
+++ apps/spotify-web/src/api/Auth/AuthService.ts
@@ -1,8 +1,25 @@
+import axios from "axios";
+
 class AuthService {
   url = "http://localhost:3000";
   async GoogleAuth(_: any, thunkAPI: any) {
     try {
       window.location.href = "http://localhost:3000/auth/google/login";
+    } catch (err: any) {
+      return thunkAPI.rejectWithValue(err.message || "Unknown error");
+    }
+  }
+  async Login(email: string, thunkAPI: any) {
+    try {
+      let response = await axios.post(
+        "http://localhost:3000/auth/login",
+        email
+      );
+      if (response.status === 201) {
+        window.localStorage.setItem("User", JSON.stringify(response.data.user));
+        window.location.href = "/";
+        return response.data;
+      }
     } catch (err: any) {
       return thunkAPI.rejectWithValue(err.message || "Unknown error");
     }
